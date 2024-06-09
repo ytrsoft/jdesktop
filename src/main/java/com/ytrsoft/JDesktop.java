fd5c9f47b1d6451bf3aa21720fe641ed0eb35bf6
ytrsoft <1654486219@qq.com>
2024-06-09 21:10:36 +0800

    clear

--- src/main/java/com/ytrsoft/JDesktop.java
+++ src/main/java/com/ytrsoft/JDesktop.java
@@ -1,9 +1,22 @@
 package com.ytrsoft;
 
-public class JDesktop {
+import com.ytrsoft.base.Application;
+import org.jdesktop.swingx.JXButton;
+
+import java.awt.*;
+
+public class JDesktop extends Application {
+
+    @Override
+    public void start() {
+        setLayout(new FlowLayout());
+        JXButton button = new JXButton("JDesktop");
+        button.setPreferredSize(new Dimension(120, 48));
+        add(button);
+    }
 
     public static void main(String[] args) {
-        System.out.println("JDesktop");
+        launch(JDesktop.class);
     }
 
 }
---
+++ src/main/java/com/ytrsoft/base/Application.java
@@ -0,0 +1,76 @@
+package com.ytrsoft.base;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import org.jdesktop.swingx.JXFrame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.URL;
+
+public class Application extends JXFrame {
+
+    private static final double SIZE_SCALE = 0.5;
+
+    static class WindowClosing extends WindowAdapter {
+
+        private final Application app;
+
+        public WindowClosing(Application app) {
+            this.app = app;
+        }
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+            app.stop();
+            System.exit(0);
+        }
+    }
+
+    public Application() {
+        super("JDesktop", true);
+        setSize(getDefaultSize());
+        setIconImage(getLogo());
+        setLocationRelativeTo(null);
+        addWindowListener(new WindowClosing(this));
+    }
+
+    public Image getLogo() {
+        URL url = getClass().getResource("/logo.png");
+        return Toolkit.getDefaultToolkit().getImage(url);
+    }
+
+    private static Dimension getScreenSize() {
+        return Toolkit.getDefaultToolkit().getScreenSize();
+    }
+
+    public Dimension getDefaultSize() {
+        Dimension size = getScreenSize();
+        int w = (int)(size.width * SIZE_SCALE);
+        int h = (int)(size.height * SIZE_SCALE);
+        return new Dimension(w, h);
+    }
+
+
+    public void init() {};
+
+    public void start() {};
+
+    public void stop() {};
+
+    public static void launch(Class<? extends Application> clz) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                FlatDarkLaf.setup();
+                Application app = clz.getDeclaredConstructor().newInstance();
+                app.init();
+                app.start();
+                app.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+}
